832d8d3112d395023f8d313c7b7a1b8790d3b168
tomcoolnl2 <tomcoolnl@gmail.com>
2024-03-29 19:00:48 +0100

    fix tailwinf css

--- ui/src/ui.module.ts
+++ ui/src/ui.module.ts
@@ -1,11 +1,11 @@
-export * from './lib/button/button.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './lib/button/button.component';
+import { HeaderComponent } from './lib/header/header.component';
 
 @NgModule({
-	declarations: [ButtonComponent],
+	declarations: [ButtonComponent, HeaderComponent],
 	imports: [CommonModule],
-	exports: [ButtonComponent],
+	exports: [ButtonComponent, HeaderComponent],
 })
 export class UiModule {}
