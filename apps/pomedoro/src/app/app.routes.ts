35fef718aa3866798eb1487443f0384b68752397
tomcoolnl2 <tomcoolnl@gmail.com>
2024-03-29 14:01:42 +0100

    setup routing

--- apps/pomedoro/src/app/app.routes.ts
+++ apps/pomedoro/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+import { HomePageComponent } from './pages/home/home.component';
+import { TimerPageComponent } from './pages/timer/timer.component';
+import { SettingsPageComponent } from './pages/settings/settings.component';
+
+
+export const appRoutes: Route[] = [
+    { path: '', component: HomePageComponent },
+    { path: 'timer', component: TimerPageComponent },
+    { path: 'settings', component: SettingsPageComponent },
+];
---
+++ apps/pomedoro/src/app/pages/timer/timer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'app-timer',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './timer.component.html',
+	styleUrl: './timer.component.css',
+})
+export class TimerPageComponent {}
