34093250ef73f394d297d6b18b7a9e006daa17ad
tomcoolnl2 <tomcoolnl@gmail.com>
2024-03-29 13:20:01 +0100

    formatting

--- apps/pomedoro-e2e/cypress.config.ts
+++ apps/pomedoro-e2e/cypress.config.ts
@@ -3,15 +3,15 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    ...nxE2EPreset(__filename, {
-      cypressDir: 'src',
-      webServerCommands: {
-        default: 'nx run pomedoro:serve:development',
-        production: 'nx run pomedoro:serve:production',
-      },
-      ciWebServerCommand: 'nx run pomedoro:serve-static',
-    }),
-    baseUrl: 'http://localhost:4200',
-  },
+	e2e: {
+		...nxE2EPreset(__filename, {
+			cypressDir: 'src',
+			webServerCommands: {
+				default: 'nx run pomedoro:serve:development',
+				production: 'nx run pomedoro:serve:production',
+			},
+			ciWebServerCommand: 'nx run pomedoro:serve-static',
+		}),
+		baseUrl: 'http://localhost:4200',
+	},
 });
--- apps/pomedoro/src/app/app.component.ts
+++ apps/pomedoro/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+	standalone: true,
+	imports: [NxWelcomeComponent, RouterModule],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pomedoro';
+	title = 'pomedoro';
 }
