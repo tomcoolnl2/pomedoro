7764a98aa583b3faf06e73244e980792bb12195a
tomcoolnl2 <tomcoolnl@gmail.com>
2024-03-29 19:47:42 +0100

    Setup UI colors

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,4 +1,5 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const defaultTheme = require('tailwindcss/defaultTheme');
 const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
@@ -8,7 +9,27 @@ module.exports = {
 		...createGlobPatternsForDependencies(__dirname),
 	],
 	theme: {
-		extend: {},
+		colors: {
+			'tomato': {
+				'50': '#fff3f1',
+				'100': '#ffe4df',
+				'200': '#ffcec5',
+				'300': '#ffac9d',
+				'400': '#ff7c64',
+				'500': '#ff6347',
+				'600': '#ed3615',
+				'700': '#c8290d',
+				'800': '#a5260f',
+				'900': '#882614',
+				'950': '#4b0f04',
+			},
+		},
+		extend: {
+			fontFamily: {
+				dancingScript: ['"Dancing Script"', ...defaultTheme.fontFamily.sans],
+				confortaa: ['"Comfortaa"', ...defaultTheme.fontFamily.sans],
+			}
+		},
 	},
 	plugins: [],
 };
