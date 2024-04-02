761d628aab2e897d915b8274774831605e54604a
tomcoolnl2 <tomcoolnl@gmail.com>
2024-04-02 08:38:13 +0200

    fix tailwind inherritance

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -2,34 +2,33 @@ const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const defaultTheme = require('tailwindcss/defaultTheme');
 const { join } = require('path');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-		...createGlobPatternsForDependencies(__dirname),
-	],
-	theme: {
-		colors: {
-			'tomato': {
-				'50': '#fff3f1',
-				'100': '#ffe4df',
-				'200': '#ffcec5',
-				'300': '#ffac9d',
-				'400': '#ff7c64',
-				'500': '#ff6347',
-				'600': '#ed3615',
-				'700': '#c8290d',
-				'800': '#a5260f',
-				'900': '#882614',
-				'950': '#4b0f04',
-			},
-		},
-		extend: {
-			fontFamily: {
-				dancingScript: ['"Dancing Script"', ...defaultTheme.fontFamily.sans],
-				confortaa: ['"Comfortaa"', ...defaultTheme.fontFamily.sans],
-			}
-		},
-	},
-	plugins: [],
+    content: [
+        join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+        ...createGlobPatternsForDependencies(__dirname),
+    ],
+    theme: {
+        extend: {
+            colors: {
+                tomato: {
+                    '50': '#fff3f1',
+                    '100': '#ffe4df',
+                    '200': '#ffcec5',
+                    '300': '#ffac9d',
+                    '400': '#ff7c64',
+                    '500': '#ff6347',
+                    '600': '#ed3615',
+                    '700': '#c8290d',
+                    '800': '#a5260f',
+                    '900': '#882614',
+                    '950': '#4b0f04',
+                },
+            },
+            fontFamily: {
+                dancingScript: ['"Dancing Script"', ...defaultTheme.fontFamily.sans],
+                confortaa: ['"Comfortaa"', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
 };
