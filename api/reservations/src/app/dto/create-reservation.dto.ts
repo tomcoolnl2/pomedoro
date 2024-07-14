76ada4d1735589854dd4eedd704649254f7a1060
tomcoolnl2 <tomcoolnl@gmail.com>
2024-07-14 08:17:37 +0200

    Added Swagger examples from DTO

--- api/reservations/src/app/dto/create-reservation.dto.ts
+++ api/reservations/src/app/dto/create-reservation.dto.ts
@@ -1,16 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateReservationDto {
 	//
+	@ApiProperty({ example: '2021-01-01T00:00:00.000Z' })
 	@IsDate()
 	@Type(() => Date)
 	public startDate: Date;
 
+	@ApiProperty({ example: '2021-01-02T00:00:00.000Z' })
 	@IsDate()
 	@Type(() => Date)
 	public endDate: Date;
 
+	@ApiProperty({ example: '1' })
 	@IsString()
 	@IsNotEmpty()
 	public invoiceId: string;
